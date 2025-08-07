5a58afd96fafa96f74b7af57258c4f79c24d3877
raileats <raileats@gmail.com>
2025-08-08 04:35:12 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header(){
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <div className="logo">RailEats</div>
+        <div className="actions">
+          <button className="btn small">Login</button>
+        </div>
+      </div>
+    </header>
+  )
+}
