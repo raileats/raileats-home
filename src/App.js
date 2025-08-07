676b90f28b27b6a9800ce4d68bae80d8b5235998
raileats <raileats@gmail.com>
2025-08-08 04:31:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-// src/App.js
 import React from "react";
 import HeroBanner from "./components/HeroBanner";
+import OffersSlider from "./components/OffersSlider";
 import CategoryCards from "./components/CategoryCards";
+import SearchByPNR from "./components/SearchByPNR";
+import SearchByTrain from "./components/SearchByTrain";
 import PopularItems from "./components/PopularItems";
+import Footer from "./components/Footer";
 import "./index.css";
 
 function App() {
   return (
-    <div>
+    <div style={{ backgroundColor: "#FFF3CD", minHeight: "100vh" }}>
+      {/* Top Banner */}
       <HeroBanner />
+
+      {/* Offers Slider */}
+      <OffersSlider />
+
+      {/* Categories */}
       <CategoryCards />
+
+      {/* Search Sections */}
+      <SearchByPNR />
+      <SearchByTrain />
+
+      {/* Popular Items */}
       <PopularItems />
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
