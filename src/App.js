551ada2dc649fde44f741b03d1cbad1c27ffa614
raileats <raileats@gmail.com>
2025-08-08 04:41:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeroBanner from "./components/HeroBanner";
 import OffersSlider from "./components/OffersSlider";
 import CategoryCards from "./components/CategoryCards";
@@ -6,30 +7,31 @@ import SearchByPNR from "./components/SearchByPNR";
 import SearchByTrain from "./components/SearchByTrain";
 import PopularItems from "./components/PopularItems";
 import Footer from "./components/Footer";
-import "./index.css";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div style={{ backgroundColor: "#FFF3CD", minHeight: "100vh" }}>
-      {/* Top Banner */}
-      <HeroBanner />
-
-      {/* Offers Slider */}
-      <OffersSlider />
-
-      {/* Categories */}
-      <CategoryCards />
-
-      {/* Search Sections */}
-      <SearchByPNR />
-      <SearchByTrain />
-
-      {/* Popular Items */}
-      <PopularItems />
-
-      {/* Footer */}
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div style={{ backgroundColor: "#FFF3CD", minHeight: "100vh" }}>
+              <HeroBanner />
+              <OffersSlider />
+              <CategoryCards />
+              <SearchByPNR />
+              <SearchByTrain />
+              <PopularItems />
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
